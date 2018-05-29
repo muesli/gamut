ea4a55227ac95cb780e626fddc1fcdc87d96ab56
Christian Muehlhaeuser <muesli@gmail.com>
2018-05-29 12:53:03 +0200

    Added palette generator example

---
+++ examples/generator/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+
+	colorful "github.com/lucasb-eyer/go-colorful"
+	"github.com/muesli/gamut"
+)
+
+var (
+	header = `
+	<html>
+	<body>
+		<table width="100%" height="100%">
+			<tr>
+	`
+
+	cell = `
+				<td bgcolor="%s" />
+	`
+
+	footer = `
+			</tr>
+		</table>
+	</body>
+	</html>
+	`
+)
+
+func main() {
+	// Write HTML header
+	buffer := bytes.NewBuffer([]byte{})
+	buffer.Write([]byte(header))
+
+	cc, err := gamut.Generate(8, gamut.Pastel{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, c := range cc {
+		col, ok := colorful.MakeColor(c)
+		if !ok {
+			panic(fmt.Sprintf("invalid RGB color: %s", c))
+		}
+
+		fmt.Println("Color as Hex:", col.Hex())
+		buffer.Write([]byte(fmt.Sprintf(cell, col.Hex())))
+	}
+
+	// Write HTML footer and generate palette.html
+	buffer.Write([]byte(footer))
+	ioutil.WriteFile("palette.html", buffer.Bytes(), 0644)
+}
