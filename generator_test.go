3ab61be8532f852c2d0ba6c4858e186001349bc7
Christian Muehlhaeuser <muesli@gmail.com>
2018-06-09 15:01:31 +0200

    Added tests for various ColorGenerators

--- generator_test.go
+++ generator_test.go
@@ -2,16 +2,40 @@ package gamut
 
 import (
 	"testing"
+
+	colorful "github.com/lucasb-eyer/go-colorful"
 )
 
+type TestColorGenerator struct {
+	BroadGranularity
+}
+
+func (cc TestColorGenerator) Valid(col colorful.Color) bool {
+	l, _, _ := col.Lab()
+	return 0.4 <= l && l <= 0.6
+}
+
 func TestGenerator(t *testing.T) {
-	cc, err := Generate(8, PastelGenerator{})
-	if err != nil {
-		t.Error(err)
-		return
+	tt := []struct {
+		generator ColorGenerator
+		amount    int
+	}{
+		{PastelGenerator{}, 8},
+		{WarmGenerator{}, 8},
+		{HappyGenerator{}, 8},
+		{SimilarHueGenerator{Color: Hex("#ABCDEF")}, 8},
+		{TestColorGenerator{}, 8},
 	}
 
-	if len(cc) != 8 {
-		t.Errorf("Expected 8 colors from generator, got %d", len(cc))
+	for _, g := range tt {
+		cc, err := Generate(g.amount, g.generator)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		if len(cc) != g.amount {
+			t.Errorf("Expected %d colors from generator, got %d", g.amount, len(cc))
+		}
 	}
 }
